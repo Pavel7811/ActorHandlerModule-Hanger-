b4d4b7bba5b1e689775724d6d2d297aa3209a542
Pavel7811 <62295683+Pavel7811@users.noreply.github.com>
2021-06-03 19:14:23 +0300

    залил

---
+++ ActorHandlerModule/ActorHandlerModuleHunger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using ActorModule;
+using OSMLSGlobalLibrary.Modules;
+using NetTopologySuite.Geometries;  // Отсюда Point и другая геометрия
+using NetTopologySuite.Mathematics; // Отсюда векторы
+using NodaTime;
+using NodaTime.Extensions;
+using PathsFindingCoreModule;
+using InitializeActorModule;
+using ActorHandlerModule;
+
+namespace ActorHandlerModuleHunger 
+{
+    public class ActorHandlerModuleHunger : OSMLSModule
+    {
+        public LocalTime HungerTimeStart;
+        public LocalTime HungerTimeEnd;
+
+        /// <summary>
+        /// Инициализация модуля. В отладочной конфигурации выводит сообщение
+        /// </summary>
+        protected override void Initialize()
+        {
+            Console.WriteLine("ActorHandlerModuleHunger: Initialize");
+        }
+        /// <summary>
+        /// Вызывает Update на всех акторах
+        /// </summary>
+        public override void Update(long elapsedMilliseconds)
+        {
+            var actors = MapObjects.GetAll<Actor>();
+
+            foreach (var actor in actors)
+            {
+                //Установка приоритета
+                int newPriority = 0;
+
+                //установлена ли уже активность у актора
+                bool isActivity = actor.Activity != null;
+                //Является ли активность нашей
+                bool isMovementActivityHunger = actor.Activity is MovementActivityHunger;
+                bool isWaitingActivityHunger = actor.Activity is WaitingActivityHunger;
+
+
+                Console.WriteLine($"Flags: IsActivity={isActivity} IsActivityMovement={isMovementActivityHunger} IsActivityWaiting={isWaitingActivityHunger}");
+                //Если активность не установлена или приоритет Активностей выше
+                if ((!isActivity) || (!isMovementActivityHunger && !isWaitingActivityHunger && newPriority > actor.Activity.Priority))
+                {
+                    // Назначить актору путь до места употребления пищи
+                    actor.Activity = new MovementActivityHunger(newPriority, new TimeInterval(HungerTimeStart, HungerTimeEnd));
+                    Console.WriteLine("Said actor go hunger\n");
+                }
+            }
+        }
+    }
+}
+
---
+++ ActorHandlerTestModuleHunger/SampleState.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using NetTopologySuite.Geometries;
+
+namespace ActorModule
+{
+    // Реализуем интерфейс компонента состояния
+    public class SampleState : IState
+    {
+        // Голод
+        public int Hunger;
+
+        // Место работы
+        public Point Job;
+
+        // Время работы
+        public TimeInterval JobTime;
+
+        // Расположение дома
+        public Point Home;
+
+        // Интервал времени, когда актор находится дома
+        public TimeInterval HomeTime;
+
+        // Список любимых мест актора
+        public readonly List<Geometry> FavoritePlaces;
+
+        // Создает пустое состояние
+        public SampleState()
+        {
+            FavoritePlaces = new List<Geometry>();
+        }
+
+        // Конструктор копирования
+        public SampleState(SampleState state)
+        {
+            // Исключение, если копируемое состояние - null
+            if (state == null)
+                throw new ArgumentNullException("state");
+
+            Hunger = state.Hunger; //Int неизменяемый
+
+            // Точки на карте вроде никуда не убегут
+            Job = state.Job;
+            Home = state.Home;
+
+            // Интервалы теперь readonly struct, так что копирования не требуется
+            JobTime = state.JobTime;
+            HomeTime = state.HomeTime;
+
+            // Список тоже изменяемый, поэтому нужен новый список с его элементами
+            FavoritePlaces = new List<Geometry>(state.FavoritePlaces);
+
+        }
+
+        // Выполняет копирование компонента, необходим для соответствия интерфейсу
+        public IState Copy()
+        {
+            return new SampleState(this);
+        }
+    }
+}
